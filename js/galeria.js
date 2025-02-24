446b9007a02f1c62470176bef8a1d428a85ad52c
AdrianMaseda <adrianmasedabulnes@gmail.com>
2025-02-24 16:33:18 +0100

    Ajustes de diseño
    Añadida una pequeña ayuda para el usuario en el presupuesto, y añadida una barra de progreso en el slider de imágenes de la galería.

--- js/galeria.js
+++ js/galeria.js
@@ -1,3 +1,5 @@
+/*  Se añade un listener al cargar la web para que se cree un Swiper en la galería, que tendrá
+    efecto slide (deslizamiento) de las imágenes.*/
 document.addEventListener("DOMContentLoaded", function() {
     var swiper = new Swiper(".swiper-container", {
         loop: true, // Infinito
@@ -8,5 +10,24 @@ document.addEventListener("DOMContentLoaded", function() {
             delay: 3000, //Cambia cada 3 segundos
             disableOnInteraction: false, //Sigue operando aunque estemos, por ejemplo, en hover
         },
+        on: {
+            slideChange: function () {
+                actualizarBarra();
+            }
+        }
     });
+
+    /*  Función para que se muestre un porcentaje de progresión en una barra debajo de las imágenes
+        de la galería.*/
+    function actualizarBarra()  {
+        const barra = document.getElementById("barra-progreso");
+        const totalSlides = document.querySelectorAll(".swiper-slide").length;
+        const indice = swiper.realIndex + 1;
+        const progreso = (indice / totalSlides) * 100;
+        barra.style.width = `${progreso}%`;
+    }
+
+    actualizarBarraProgreso();
+
 });
+
